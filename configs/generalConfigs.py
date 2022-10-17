76c581845d880ad50ecb7f143055fc8048fae245
matto <mattiabellotti.dev@gmail.com>
2022-10-17 12:11:27 +0200

    windows fix

--- configs/generalConfigs.py
+++ configs/generalConfigs.py
@@ -1,3 +1,16 @@
-RESET = "\x1b[0m"
-GREEN = "\x1b[32;20m"
-RED = "\x1b[31;20m"
+import sys
+
+platform = sys.platform
+
+
+if('win' in platform):
+    RESET = "[0m"
+    GREEN = "^<ESC^>[32m [32m"
+    RED = "^<ESC^>[31m [31m"
+elif('linux' in platform):
+    RESET = "\x1b[0m"
+    GREEN = "\x1b[32;20m"
+    RED = "\x1b[31;20m"
+
+
+
--- startAll.py
+++ startAll.py
@@ -20,6 +20,6 @@ for symbol in symbols:
     if('linux' in platform):
         cmd = 'gnome-terminal --title=' + symbol['symbol'] + ' -- python XTBot.py ' + symbol['symbol']
     elif('win' in platform):
-        cmd = 'start "' +  symbol['symbol'] + '"cmd /c python XTBot.py ' + symbol['symbol']
+        cmd = 'start "' +  symbol['symbol'] + ' "cmd /c python XTBot.py ' + symbol['symbol']
 
     os.system(cmd)
