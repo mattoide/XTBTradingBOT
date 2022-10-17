bb9d3e5558bae914628e972ecba8d00a3d7d2817
matto <mattiabellotti.dev@gmail.com>
2022-10-17 14:52:58 +0200

    fix stopall windown

--- stopAll.py
+++ stopAll.py
@@ -9,10 +9,7 @@ from configs.symbolConfig import getAllSymbols
 
 symbols = json.loads(getAllSymbols())
 
-for symbol in symbols:
-    cmd = 'kill -9 $(ps -u | grep "XTBot.py ' + symbol['symbol'] + '" | awk \'{print $2}\' | head -n 1)'
-    # print(cmd)
-    os.system(cmd)
+cmd = ''
 
 for symbol in symbols:
     if('linux' in platform):
