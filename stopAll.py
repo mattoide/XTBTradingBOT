203d0b46d7f507f58b52e8b79076427c9bd0f5a9
matto <mattiabellotti.dev@gmail.com>
2022-10-17 14:48:06 +0200

    fix stopall windown

--- stopAll.py
+++ stopAll.py
@@ -1,5 +1,8 @@
 import os
 import json
+import sys
+
+platform = sys.platform
 
 
 from configs.symbolConfig import getAllSymbols
@@ -9,4 +12,13 @@ symbols = json.loads(getAllSymbols())
 for symbol in symbols:
     cmd = 'kill -9 $(ps -u | grep "XTBot.py ' + symbol['symbol'] + '" | awk \'{print $2}\' | head -n 1)'
     # print(cmd)
+    os.system(cmd)
+
+for symbol in symbols:
+    if('linux' in platform):
+        cmd = 'kill -9 $(ps -u | grep "XTBot.py ' + symbol['symbol'] + '" | awk \'{print $2}\' | head -n 1)'
+    elif('win' in platform):
+        s = symbol['symbol']
+        cmd = f'for /F "tokens=1,2" %A in (\'\"tasklist /FI \"WINDOWTITLE eq {s}\" /FO LIST | FIND \"PID\" \"\') DO TASKKILL /pid %B'
+
     os.system(cmd)
